d0265cc6e2740d9e4e56ab00992a65a935d773fb
wenzhi <2271034209@qq.com>
2024-06-07 02:24:54 +0800

    删除

--- node_modules/.pnpm/nanopop@2.4.2/node_modules/nanopop/dist/tests/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { Page } from '@playwright/test';
-export declare const positions: string[];
-export declare const variants: string[];
-export declare const testPage: (page: Page, name: string) => Promise<void>;
